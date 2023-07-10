b2867ebe0c2e017c4f9143ff7b37e864c062c596
web3amrit <138011372+web3amrit@users.noreply.github.com>
2023-07-10 12:22:38 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,57 +1,17 @@
 import logging
 from PIL import Image
 import io
-from typing import List
+
 from fastapi import FastAPI, HTTPException, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
 import dai
 import quickstart
-
-# Database related imports
-# import pyodbc
-
-# Azure SQL DB connection string
-# SERVER = 'your_server.database.windows.net'
-# DATABASE = 'your_database'
-# USERNAME = 'your_username'
-# PASSWORD = 'your_password'
-# DRIVER = '{ODBC Driver 17 for SQL Server}'
-
-# Class definitions for handling database operations
-# class Database:
-#     def __init__(self):
-#         self.conn = pyodbc.connect(f'DRIVER={DRIVER};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}')
-#         self.cursor = self.conn.cursor()
-
-#     def execute(self, statement):
-#         self.cursor.execute(statement)
-#         self.conn.commit()
-
-#     def fetchone(self, statement):
-#         self.cursor.execute(statement)
-#         return self.cursor.fetchone()
-
-#     def fetchall(self, statement):
-#         self.cursor.execute(statement)
-#         return self.cursor.fetchall()
-
-# db = Database()
-
-class Message(BaseModel):
-    user_id: str
-    content: str
-
-class Conversation(BaseModel):
-    id: str
-    user_id: str
-    messages: List[Message]
 
 app = FastAPI()
 app = FastAPI(debug=True)
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)  # Change to DEBUG for more detailed log
 
 allowed_origins = [
     "http://localhost:8000",
@@ -146,32 +106,3 @@ async def generate_statements(query: str):
     except Exception as e:
         logging.exception("Unexpected error occurred.")
         return JSONResponse(status_code=500, content={"message": f"Unexpected error occurred: {str(e)}"})
-
-# Below are the new API endpoints related to conversations and messages. Database calls have been commented out.
-
-@app.post("/conversations", response_model=Conversation)
-async def create_conversation(user_id: str):
-    # db.execute(f"INSERT INTO Conversations (user_id) VALUES ('{user_id}')")
-    # conversation = db.fetchone(f"SELECT * FROM Conversations WHERE user_id='{user_id}'")
-    # return Conversation(**conversation)
-    pass
-
-@app.get("/conversations/{conversation_id}", response_model=Conversation)
-async def get_conversation(conversation_id: str):
-    # conversation = db.fetchone(f"SELECT * FROM Conversations WHERE id='{conversation_id}'")
-    # messages = db.fetchall(f"SELECT * FROM Messages WHERE conversation_id='{conversation_id}'")
-    # conversation['messages'] = messages
-    # return Conversation(**conversation)
-    pass
-
-@app.post("/conversations/{conversation_id}/messages", response_model=Message)
-async def create_message(conversation_id: str, message: Message):
-    # db.execute(f"INSERT INTO Messages (conversation_id, user_id, content) VALUES ('{conversation_id}', '{message.user_id}', '{message.content}')")
-    # return message
-    pass
-
-@app.get("/conversations/{conversation_id}/messages", response_model=List[Message])
-async def get_messages(conversation_id: str):
-    # messages = db.fetchall(f"SELECT * FROM Messages WHERE conversation_id='{conversation_id}'")
-    # return messages
-    pass
