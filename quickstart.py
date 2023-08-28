a91946a8576a29a93d16dd61f15179c240d078b8
web3amrit <138011372+web3amrit@users.noreply.github.com>
2023-08-28 16:05:44 -0400

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -51,16 +51,16 @@ async def custom_rate_limit_handler(request, exc):
 
 @app.on_event("startup")
 async def startup_event():
-    logger.info("Application startup")
+    logging.info("Application startup")
     app.state.session = aiohttp.ClientSession()
 
 @app.on_event("shutdown")
 async def shutdown_event():
-    logger.info("Application shutdown")
+    logging.info("Application shutdown")
     await app.state.session.close()
 
 async def convert_image(file: UploadFile):
-    logger.info(f"Converting image: {file.filename}")
+    logging.info(f"Converting image: {file.filename}")
     try:
         image = Image.open(file.file)
         if file.content_type == "image/heif":
@@ -76,7 +76,7 @@ async def convert_image(file: UploadFile):
         logger.error(f"Failed to convert image: {str(e)}")
         raise HTTPException(status_code=415, detail=f"Could not process the file: {str(e)}")
     
-    logger.info(f"Image conversion completed: {file.filename}")
+    logging.info(f"Image conversion completed: {file.filename}")
     return UploadFile(
         filename=file.filename,
         file=file.file
@@ -95,7 +95,7 @@ async def upload_image_to_blob(file: UploadFile):
     container_client = blob_service_client.get_container_client(container_name)  # Get container client
     blob_client = container_client.get_blob_client(blob_name)
 
-    logger.info(f"Checking if the blob: {blob_name} already exists")
+    logging.info(f"Checking if the blob: {blob_name} already exists")
     try:
         blob_client.get_blob_properties()
         logger.warning(f"File: {file.filename} already exists in blob storage.")
@@ -103,7 +103,7 @@ async def upload_image_to_blob(file: UploadFile):
     except ResourceNotFoundError:
         pass
 
-    logger.info(f"Starting to upload file: {file.filename} to blob: {blob_name}")
+    logging.info(f"Starting to upload file: {file.filename} to blob: {blob_name}")
     block_list = []
     try:
         chunk_size = 1024 * 1024 * 4  # 4MB
@@ -122,7 +122,7 @@ async def upload_image_to_blob(file: UploadFile):
             block_id += 1
 
         blob_client.commit_block_list(block_list)
-        logger.info(f"Successfully uploaded file: {file.filename} to blob: {blob_name}")
+        logging.info(f"Successfully uploaded file: {file.filename} to blob: {blob_name}")
 
     except Exception as e:
         logger.error(f"Failed to upload image to blob: {str(e)}")
@@ -137,7 +137,7 @@ async def create_upload_file(file: UploadFile = File(...)):
         logger.error(f"Invalid file type for file: {file.filename}")
         raise HTTPException(status_code=400, detail="Invalid file type. Only JPEG, PNG, and HEIF types are allowed.")
     
-    logger.info(f"Received file: {file.filename} for upload and conversion")
+    logging.info(f"Received file: {file.filename} for upload and conversion")
     try:
         file = await convert_image(file)
         image_url = await upload_image_to_blob(file)
@@ -148,7 +148,7 @@ async def create_upload_file(file: UploadFile = File(...)):
 
     image_dict = {"image_url": image_url, "file_name": file.filename}
 
-    logger.info(f"Sending processed image data to DAI service")
+    logging.info(f"Sending processed image data to DAI service")
     try:
         response = await dai.receive_uploaded_image(image_dict)
         return response
@@ -159,4 +159,4 @@ async def create_upload_file(file: UploadFile = File(...)):
             detail=generic_error_message
         )
     
-logger.info("Service is running...")
+logging.info("Service is running...")
